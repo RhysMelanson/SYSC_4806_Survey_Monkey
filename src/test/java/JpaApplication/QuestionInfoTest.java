d7f155e5dfa71445eb4d50451f8553094e87645b
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2021-03-11 13:11:12 -0500

    Create QuestionInfoTest.java

---
+++ src/test/java/JpaApplication/QuestionInfoTest.java
@@ -0,0 +1,56 @@
+package JpaApplication;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class QuestionInfo {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String question = "";
+    private String answer = "";
+
+    @ManyToOne
+    private Survey survey;
+
+    public QuestionInfo(){}
+
+    public QuestionInfo(String question) {
+        this.question = question;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        String s = "The answer to the Question: " + question + " is: \n" + answer;
+        return s;
+    }
+}
