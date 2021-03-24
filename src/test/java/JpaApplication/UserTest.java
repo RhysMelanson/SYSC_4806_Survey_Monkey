bb16420dbf4dfbaf5e963e598b58bfc8bb133617
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2021-03-23 21:47:55 -0400

    Create UserTest.java

---
+++ src/test/java/JpaApplication/UserTest.java
@@ -0,0 +1,24 @@
+package JpaApplication;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class UserTest {
+    @Test
+    public void testId(){
+        User user = new User("test");
+        user.setId(52);
+        assertEquals(52, user.getId());
+    }
+    @Test
+    public void testGetUserName(){
+        User user = new User("testName");
+        assertEquals("testName", user.getUserName());
+    }
+    @Test
+    public void testSetUserName(){
+        User user = new User("test1");
+        user.setUserName("test2");
+        assertEquals("test2", user.getUserName());
+    }
+}
