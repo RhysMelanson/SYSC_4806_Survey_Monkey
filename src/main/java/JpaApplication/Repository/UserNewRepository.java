7121b34cc905676db3b5a03a120e351744c2c51a
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2021-04-06 18:56:47 -0400

    Update UserNewRepository.java

--- src/main/java/JpaApplication/Repository/UserNewRepository.java
+++ src/main/java/JpaApplication/Repository/UserNewRepository.java
@@ -4,7 +4,7 @@ import JpaApplication.Model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "userNew", path = "userNew")
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserNewRepository extends CrudRepository<User, Long> {
 
     User findById(long id);
