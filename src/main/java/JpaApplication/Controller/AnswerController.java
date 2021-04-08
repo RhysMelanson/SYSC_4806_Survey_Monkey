2655231716700584384e2a241abccaa888a9c61d
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2021-04-08 18:19:05 -0400

    Update AnswerController.java

--- src/main/java/JpaApplication/Controller/AnswerController.java
+++ src/main/java/JpaApplication/Controller/AnswerController.java
@@ -1,10 +1,13 @@
 package JpaApplication.Controller;
 
 import JpaApplication.Repository.AnswerRepository;
-import JpaApplication.Model.Answer;
+import JpaApplication.Model.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,4 +24,22 @@ class AnswerController {
     List<Answer> all() {
         return (List<Answer>) repository.findAll();
     }
+
+    @GetMapping("/answerPieInfo")
+    public String getData(Model model, @ModelAttribute Question question, @ModelAttribute String answer)
+    {
+        List<List<Object>> allData = new ArrayList<List<Object>>();
+        List answers;
+        answers = question.getAnswers();
+        for(int i = 0;i<answers.size();i++)
+        {
+            List<Object> data = new ArrayList<Object>();
+            data.add(answers.get(i));
+            data.add(repository.findByAnswerAndQuestion(answer, question));
+            allData.add(data);
+        }
+        model.addAttribute("pieData", allData);
+        return "pieChart";
+    }
+
 }
