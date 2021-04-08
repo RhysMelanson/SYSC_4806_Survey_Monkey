b454a191c045db9c53f9aead8d67b74773acb146
YusufH <hassan.yusuf@canada.ca>
2021-04-08 16:22:51 -0400

    changed addQuestion page to be a single page app

--- src/main/java/JpaApplication/Repository/QuestionRepository.java
+++ src/main/java/JpaApplication/Repository/QuestionRepository.java
@@ -1,7 +1,9 @@
 package JpaApplication.Repository;
 
 import JpaApplication.Model.Question;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "questions", path = "questions")
